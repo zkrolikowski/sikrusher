380c1285d446c41463c6f94bdc55bf7e1e8fe9ca
Zack <39175158+zkrolikowski@users.noreply.github.com>
2022-05-24 21:09:11 -0500

    [+] Added subtraction blend mode

--- Crusher/Cursher.py
+++ Crusher/Cursher.py
@@ -1,7 +1,8 @@
-from PIL import Image    # Image processeing
-import argparse          # Command line arguments
-import numpy as np       # subtracting images
-import blend_modes as bm # blend modes
+#!
+from PIL import Image                       # Image processeing
+import argparse                             # Command line arguments
+from BlendModes import SIK_Blends as blends # blend modes
+import blend_modes as bm                    # for testing
 
 # Command line arguments
 parser = argparse.ArgumentParser(description='Crush an image')
@@ -13,21 +14,10 @@ args = parser.parse_args()
 # load and show image
 with Image.open(args.image) as image:
     with Image.open(args.crusher) as crusher:
-        
+
+        # subtract the images
         crusher = crusher.resize(image.size)
 
-        # subtract the crusher from the image
-        npImage = np.array(image)
-        npCrusher = np.array(crusher)
-
-
-        npImageFloat = npImage.astype(np.float32)
-        npCrusherFloat = npCrusher.astype(np.float32)
-
-
-        npImage = bm.difference(npCrusherFloat, npImageFloat, opacity = 0.3)
-
-        # convert the image back to a PIL image
-        npImage = npImage.astype(np.uint8)
-        newImage = Image.fromarray(npImage)
+        newImage = blends.subtract(image, crusher, 1.0)
+        
         newImage.show()

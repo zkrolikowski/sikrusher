1bb1f1400b381a1713195b3f9a4e043d92baceae
mini-sneps <99615689+mini-sneps@users.noreply.github.com>
2022-05-26 18:49:37 -0700

    [~] Fixing Electron Merge

--- MainGUI/src/main.js
+++ MainGUI/src/main.js
@@ -4,28 +4,38 @@ const path = require('path')
 const { isMainThread } = require('worker_threads')
 
 function createWindow() {
-  // Create the browser window.
+
+  // The main GUI application settings
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minHeight: 300,
+    minWidth: 400,
     show: false,
+    frame: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
   mainWindow.loadFile('app/index.html')
-  // and load the index.html of the app.
+  mainWindow.webContents.openDevTools()
+  mainWindow.setMenuBarVisibility(false)
+ 
+  // The splash window settings
   var splash = new BrowserWindow({ 
     width: 800, 
     height: 800, 
     transparent: true, 
     frame: false, 
-    alwaysOnTop: true 
+    alwaysOnTop: true,
+    resizable: false
   });
   
+  // Display the splash window for 5 secs
   splash.loadFile('app/splash.html');
   splash.center();
-
   setTimeout(function () {
     splash.close();
     mainWindow.center();
@@ -33,16 +43,16 @@ function createWindow() {
   }, 5000);
 }
 
+// Display Main GUI
 app.whenReady().then(() => {
   createWindow()
-
-
   app.on('activate', function () {
 
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
+// closes all windows if all tabs are closed. For Windows and linux
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })

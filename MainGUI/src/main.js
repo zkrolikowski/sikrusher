db8d8c48d1a29fe44ff74f9c1186f5f3fb973ab4
mini-sneps <99615689+mini-sneps@users.noreply.github.com>
2022-06-01 02:53:34 -0700

    [+] Added filters box + cleaned up code/directory

--- MainGUI/src/main.js
+++ MainGUI/src/main.js
@@ -2,15 +2,13 @@
 const { app, BrowserWindow, ipcMain, dialog, remote } = require('electron')
 const path = require('path') //used for preload
 
-// download the edited image applying all filters.
-function downloadImage() {
-  console.log("download meow!")
-}
-
+//========================================
+//        GUI DISPLAY FUNCTIONS
+//========================================
 // opens up the application
 function createWindow() {
-    // The main GUI application settings
-    const mainWindow = new BrowserWindow({
+  // The main GUI application settings
+  const mainWindow = new BrowserWindow({
     width: 1400,
     height: 900,
     minHeight: 530,
@@ -23,9 +21,9 @@ function createWindow() {
       contextIsolation: true
     }
   })
-  mainWindow.loadFile('app/index.html')
-  //mainWindow.webContents.openDevTools()
-  mainWindow.setMenuBarVisibility(false)
+  mainWindow.loadFile('src/html/index.html')
+  mainWindow.setMenuBarVisibility(false) //hide the menu bar
+  mainWindow.webContents.openDevTools()
 
   // The splash window settings
   var splash = new BrowserWindow({
@@ -37,15 +35,17 @@ function createWindow() {
     resizable: false
   });
 
-  // Display the splash window for 5 secs
-  splash.loadFile('app/splash.html');
+  // Display the splash window for 1 second, or until main window loads
+  splash.loadFile('src/html/splash.html');
   splash.center();
-  setTimeout(function () {
-    splash.close();
-    mainWindow.center();
-    mainWindow.show();
-  }, 5000);
-}
+  mainWindow.once('ready-to-show', () => {
+    setTimeout(function () {
+      splash.close();
+      mainWindow.center();
+      mainWindow.show();
+    }, 1000);
+  });
+} // end of createWindow()
 
 // Display Main GUI
 app.whenReady().then(() => {
@@ -61,17 +61,16 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+}) // end of displaying main GUI
 
 // closes all windows if all tabs are closed. For Windows and linux
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
-})
+}) // end of closing all windows
 
 //========================================
 //        FUNCTIONS FOR RENDERER
 //========================================
-
 // quit the application
 function quitProgram() {
   app.quit()
@@ -92,17 +91,26 @@ function maxmimizeProgram() {
     window.unmaximize()
 }
 
-// allow user to change the image to edit
+// Allow the user to change the displayed image
+//@params: event, the event that triggered the function
+//@params: oldpath, the current source/path of the image
+//@return: oldpath, the current source/path of the image
+//@return: filePaths[0], the new source/path of the image user selected
 async function addImage(event, oldPath) {
   // opens the file explorer searching for image formats. will return image
-  const { canceled, filePaths } =  await dialog.showOpenDialog(
+  const { canceled, filePaths } = await dialog.showOpenDialog(
     BrowserWindow.getFocusedWindow(),
     {
-    properties: ["openFile"],
-    filters: [{ name: "Images", extensions: ["png", "jpg", "jpeg"] }]
-  });
+      properties: ["openFile"],
+      filters: [{ name: "Images", extensions: ["png", "jpg", "jpeg"] }]
+    });
   if (canceled)
     return oldPath;
   else
     return filePaths[0]
+}
+
+// download the edited image applying all filters.
+function downloadImage() {
+  console.log("download meow!")
 }
--- MainGUI/src/renderer.js
+++ MainGUI/src/renderer.js
@@ -7,37 +7,55 @@ const addImageButton1 = document.getElementById('add-img-button-1')
 const addImageButton2 = document.getElementById('add-img-button-2')
 const userImagePath = document.getElementById('user-image')
 const downloadImageButton = document.getElementById('download-img-button')
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-// These will call the signals that were configured in preload
-// Quit the program on click
+//========================================
+//        PRELOAD SIGNAL FUNCTIONS
+//========================================
+// Quit the program
 closeWinButton.addEventListener('click', () => {
     window.electronAPI.quitProgram()
 });
 
-// minimize program on click
+// Minimize program
 minimizeWinButton.addEventListener('click', () => {
     window.electronAPI.minimizeProgram()
 });
 
-// un/maximuize the program on click
+// Un/maximuize the program
 maximizeWinButton.addEventListener('click', () => {
     window.electronAPI.maximizeProgram()
 });
 
-// allow the user to add their own image to the app
-addImageButton1.addEventListener('click', async () => {
+// Change the image by double clicking the current image
+addImageButton1.addEventListener('dblclick', async () => {
     const filePath = await window.electronAPI.addImage(userImagePath.src)
     userImagePath.src = filePath //change the image
 });
 
-// allow the user to add their own image to the app
+// Change the image by clicking the add image button
 addImageButton2.addEventListener('click', async () => {
     const filePath = await window.electronAPI.addImage(userImagePath.src)
     userImagePath.src = filePath //change the image
 });
 
-// download the resulting image to the users selected choice
+// Download the current rendered image
 downloadImageButton.addEventListener('click', () => {
     window.electronAPI.downloadImage()
 });
 
+//========================================
+//             JS FUNCTIONS
+//========================================
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
